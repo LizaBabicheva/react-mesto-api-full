2d28bb2c8d5af614b44e8684c606f6aade6a4ddd
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-11-08 17:42:08 +0300

    change cors

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,10 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://mesto.lizababicheva.nomoredomains.icu/sign-up',
+    'https://mesto.lizababicheva.nomoredomains.icu/sign-up'],
+}));
 
 app.use(cookieParser());
 
