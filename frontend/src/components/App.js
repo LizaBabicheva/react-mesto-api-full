2ed7400edcf679b8bd8afe3c3d269bcc8606e780
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-11-11 13:19:41 +0300

    refactor handleCardLike

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -167,7 +167,7 @@ function App() {
   }
 
   function handleCardLike(card) {
-    const isLiked = card.likes.some(i => i.owner === currentUser._id);
+    const isLiked = card.likes.some(i => i === currentUser._id);
     api.changeLikeApi(card._id, !isLiked)
       .then((newCard) => {
         setCards((state) => state.map((c) => c._id === card._id ? newCard.data : c));
