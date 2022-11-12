d28b39a36bff09e517a027f7cd39e1e9401c1e3e
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-11-12 16:13:57 +0300

    add crash-test

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,12 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(routerUsers);
 app.use('/', auth, routerCards);
 
